75861b961ba3cd6bd1dc825c39b3026abfc8ae91
Ronald Santos <santosronald19@gmail.com>
2020-09-11 16:59:56 -0500

    primer

---
+++ main.py
@@ -0,0 +1,86 @@
+import logging
+
+from flask import current_app, flash, Flask, Markup, redirect, render_template
+from flask import request, url_for
+from google.cloud import error_reporting
+import storage
+
+
+app = Flask(__name__)
+app.config.update(
+    SECRET_KEY='secret',
+    MAX_CONTENT_LENGTH=8 * 1024 * 1024,
+    ALLOWED_EXTENSIONS=set(['xls', 'xlsx'])
+)
+
+app.debug = False
+app.testing = False
+
+# [START upload_image_file]
+def upload_image_file(img):
+    """
+    Upload the user-uploaded file to Google Cloud Storage and retrieve its
+    publicly-accessible URL.
+    """
+    if not img:
+        return None
+
+    public_url = storage.upload_file(
+        img.read(),
+        img.filename,
+        img.content_type
+    )
+
+    current_app.logger.info(
+        'Uploaded file %s as %s.', img.filename, public_url)
+
+    return public_url
+# [END upload_image_file]
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        image_url = upload_image_file(request.files.get('image'))
+        data = request.form.to_dict(flat=True)
+
+    return render_template('form.html')
+
+
+# @app.route('/books/<book_id>')
+# def view(book_id):
+#     book = firestore.read(book_id)
+#     return render_template('view.html', book=book)
+
+
+@app.route('/logs')
+def logs():
+    logging.info('Hey, you triggered a custom log entry. Good job!')
+    flash(Markup('''You triggered a custom log entry. You can view it in the
+        <a href="https://console.cloud.google.com/logs">Cloud Console</a>'''))
+    return redirect(url_for('.list'))
+
+
+@app.route('/errors')
+def errors():
+    raise Exception('This is an intentional exception.')
+
+
+# Add an error handler that reports exceptions to Stackdriver Error
+# Reporting. Note that this error handler is only used when debug
+# is False
+@app.errorhandler(500)
+def server_error(e):
+    client = error_reporting.Client()
+    client.report_exception(
+        http_context=error_reporting.build_flask_context(request))
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='0.0.0.0', port=8080, debug=True)
+# [END gae_flex_quickstart]
